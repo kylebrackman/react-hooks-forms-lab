efef75326251939bebbda7ccc9f76d1ad438e56e
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-11-09 18:20:38 -0700

    commit tree ad57abc6a9a50527925de5b36641546dabe823c8

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,37 @@
-import React from "react";
+// Action Plan:
+// 1. Look back at previous labs to see were you are able to access inputs that you typed into a form
+// 2. Go back to the application and console.log what you typed.
+// — you see what you typed into the console
+// *** look back at the stack overflow link (possible solution )
+
+
+import React, {useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [cat, setCat] = useState("Produce")
+  const [name, setName] = useState("")
+
+  function newItemSubmit(event) {
+    event.preventDefault()
+    const newItem = {
+      id: uuid(), // the `uuid` library can be used to generate a unique id
+      name: name,
+      category: cat,
+    };
+    onItemFormSubmit(newItem)
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={newItemSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={(e) => setCat(e.target.value)} value={cat}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,24 +2,29 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
-
-function ShoppingList({ items }) {
+// ● uses a prop of 'search' to display the search term in the input field
+function ShoppingList({ items, setItems }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
+  }
+
+  function handleSubmitItem(newItem) {
+    setItems([...items, newItem])
   }
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
-  });
+  }).filter((item) => item.name.includes(search))
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleSubmitItem} />
+      <Filter onCategoryChange={handleCategoryChange} search={search} onSearchChange={setSearch} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
